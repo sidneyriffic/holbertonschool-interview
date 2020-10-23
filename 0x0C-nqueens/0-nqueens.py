db45b57775e04225fff4b4e34f2847a50e456de2
Sidney Burkett <sidneyriffic@gmail.com>
2020-10-22 20:42:43 -0700

    Add files for 0x0C-nqueens.py

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Solve the nqueens problem"""
+
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+try:
+    size = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    exit(1)
+if size < 4:
+    print("N must be at least 4")
+    exit(1)
+
+
+def nqueens(size, y, queens):
+    """Solve the nqueens problem"""
+    for x in range(size):
+        threat = 0
+        for queen in queens:
+            if x == queen[1]:
+                threat = 1
+                break
+            if y - x == queen[0] - queen[1]:
+                threat = 1
+                break
+            if x - queen[1] == queen[0] - y:
+                threat = 1
+                break
+        if threat == 0:
+            queens.append([y, x])
+            if y != size - 1:
+                nqueens(size, y + 1, queens)
+            else:
+                print(queens)
+            del queens[-1]
+
+
+nqueens(size, 0, [])
---
+++ 0x0C-nqueens/README.md
@@ -0,0 +1,1 @@
+Place queens on an NxN chessboard so that no queen threatens another. Find all possible solutions.
